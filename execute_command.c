002c98c51fdf68e102a495b16c08e77f9399ca5b
Gianluca Dorelo <66336015+gdorelo@users.noreply.github.com>
2020-11-17 20:21:19 -0300

    Add files via upload

--- execute_command.c
+++ execute_command.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 /**
- * execute - function that execute all arguments in command line.
- * @args: Arguments typed into the line command.
- * @env_var: env variable
- * @path: path of the program.
- * @av: command
- * Return: No return.
- */
+* execute - function that execute all arguments in command line.
+* @args: Arguments typed into the line command.
+* @env_var: env variable
+* @path: path of the program.
+* @av: command 
+* Return: No return.
+*/
 int execute(char *args[], char *av, char **env_var, char *path)
 {
 	pid_t pid;
